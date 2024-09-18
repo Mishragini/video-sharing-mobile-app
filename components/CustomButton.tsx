639ca64342b47f890d8a325c75aadb05b4666442
Mishragini <mishragini8@gmail.com>
2024-09-18 19:00:36 +0530

    auth screens init

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,13 +1,9 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const CustomButton = ({ title, handlePress, containerStyles, textStyles, isLoading }: {
-    title: string,
-    handlePress: () => void,
-    containerStyles: string,
-    textStyles?: string,
-    isLoading?: boolean
-}) => {
+const CustomButton = ({ title, handlePress, containerStyles, textStyles, isLoading }:
+    any
+) => {
     return (
         <TouchableOpacity
             className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading ? 'opacity-50' : ''}`}
